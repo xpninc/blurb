bfd532647e5d74531e6f12ad0ccbc09a722cc164
David Mazvovsky <david.mazvovsky@gmail.com>
2024-01-07 21:48:17 +0200

    Update account.rb

--- lib/blurb/account.rb
+++ lib/blurb/account.rb
@@ -102,7 +102,7 @@ class Blurb
           '',
           '',
           site: 'https://api.amazon.com',
-          ssl: { version: :TLSv1 }
+          ssl: {:verify => false}
         )
       end
   end
